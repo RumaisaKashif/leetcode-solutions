93a2b0a76eb8c72df15e52fb9ac75d30848cfb8f
Rumaisa Kashif <142867414+RumaisaKashif@users.noreply.github.com>
2026-05-31 19:22:40 +0800

    Add solution: Intersection of Two Linked Lists

---
+++ hash-tables/intersection-of-two-linked-lists/solution.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        Set<ListNode> a = new HashSet<>();
+        while (headA != null) {
+            a.add(headA);
+            headA = headA.next;
+        }
+        while (headB != null) {
+            if (a.contains(headB)) return headB;
+            headB = headB.next;
+        }
+        return null;
+    }
+}
