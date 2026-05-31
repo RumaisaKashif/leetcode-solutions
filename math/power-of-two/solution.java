fa5047cc093d0ae611b1281b6d27d8204588c8f9
Rumaisa Kashif <142867414+RumaisaKashif@users.noreply.github.com>
2026-05-31 19:23:13 +0800

    Add solution: Power of Two

---
+++ math/power-of-two/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) { 
+        if (n <= 0) {
+            return false;
+        }
+        return helper(n);
+    }
+
+    private boolean helper(int n) {
+        if (n == 1) {
+            return true;
+        } 
+        if (n % 2 != 0) {
+            return false;
+        }
+        return helper(n/2);
+    }
+}
