255e91deea4986f251a4760cb819cfa9a18cf4a8
Rumaisa Kashif <142867414+RumaisaKashif@users.noreply.github.com>
2026-05-31 19:20:46 +0800

    Add solution: Daily Temperatures

---
+++ arrays/daily-temperatures/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
+        for i, temp in enumerate(temperatures):
+            while stack and temp > temperatures[stack[-1]]:
+               prev_day = stack.pop()
+               answer[prev_day] = i - prev_day
+            stack.append(i)
+        return answer
